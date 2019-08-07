71eaa0395ab3ce065e6ce12876c3fc666719f933
Caio Cordeiro <ccmr@cesar.school>
2019-08-07 17:26:08 -0300

    Cors is now working
    Added front-end structure

--- node-api/server.js
+++ node-api/server.js
@@ -4,17 +4,33 @@ const mongoose = require('mongoose');
 const requireDir = require('require-dir');
 
 const app = express();
-
-
 app.use((express.json()))
-//CORS ta quebrando a aplicação, não utilizar por enquanto
-// app.use(cors);
+
+// app.use(cors({
+//     credentials: true,
+//     origin: ["http://localhost:3001"]
+// }));
+
 mongoose.connect('mongodb://localhost:27017/nodeapi', {
     useNewUrlParser: true
 });
 
+const connect = mongoose.connection
+connect.once('open', () => {
+    console.log("Conectou");
+});
+
 requireDir("./src/models");
 
-app.use("/api", require("./src/routes"));
+app.options('*', cors({
+    credentials: true,
+    origin: ["http://localhost:3001"]
+}));
 
+
+app.use(cors());
+
+app.use("/api", require("./src/routes"));
 app.listen(3001);
+
+console.log('Foi'); 
